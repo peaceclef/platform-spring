b320f5edf0aec6dfa61a6103fad11b5ba1da9745
mykim <mykim@herit.net>
2025-01-07 11:26:42 +0900

    fix aop 생성

--- src/main/java/net/herit/platform/platform_spring/common/logger/aop/HttpLoggingAop.java
+++ src/main/java/net/herit/platform/platform_spring/common/logger/aop/HttpLoggingAop.java
@@ -1,5 +1,10 @@
 package net.herit.platform.platform_spring.common.logger.aop;
 
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
 public class HttpLoggingAop {
 
 }
